df42693dfd264216d34e077f16326c3b1af5ddd1
Benjie Cheong <benjie.cheong@gmail.com>
2019-05-09 02:57:04 +0800

    Added base navigation menu

--- guppit-frontend/src/App.js
+++ guppit-frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/navigation/Navigation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navigation />
+      <div id={'page-wrapper'} className={"gray-bg"}>
+        <div className={'row border-bottom'}></div>
+        <div className={'wrapper wrapper-content'}></div>
+
+      </div>
+        <div id="right-sidebar" className="">
+
+        </div>
+    </React.Fragment>
   );
 }
 
